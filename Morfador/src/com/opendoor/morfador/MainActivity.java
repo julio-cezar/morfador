b18145c0133448d7575bf691ccd3b2a1b7462b7c
Julio.araujo314 <julio.araujo314@gmail.com>
2014-07-12 19:37:06 +0000

    Acrescimo de listeners dos botões e passagem de parametro da activity main para a chama_player

--- Morfador/src/com/opendoor/morfador/MainActivity.java
+++ Morfador/src/com/opendoor/morfador/MainActivity.java
@@ -1,18 +1,16 @@
 package com.opendoor.morfador;
 
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -28,18 +26,112 @@ public class MainActivity extends ActionBarActivity {
         }
         
         
-        /*Button bt1 = (Button) this.findViewById(R.id.btA);
-        bt1.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
-				startActivity(i);
-			}
-
-	});	*/
+   
     }
 
-    
+    @Override
+    protected void onStart() {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    		final Button bt1 = (Button) findViewById(R.id.btp1);
+	        bt1.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt1.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt2 = (Button) findViewById(R.id.btp2);
+	        bt2.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt2.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt3 = (Button) findViewById(R.id.btp3);
+	        bt3.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt3.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt4 = (Button) findViewById(R.id.btp4);
+	        bt4.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt4.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt5 = (Button) findViewById(R.id.btp5);
+	        bt5.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt5.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt6 = (Button) findViewById(R.id.btp6);
+	        bt6.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt6.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        final Button bt7 = (Button) findViewById(R.id.btp7);
+	        bt7.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt7.getText().toString();
+					Intent g = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					g.putExtra("modalidade", texto);
+					startActivity(g);
+				} 
+
+		});	
+	        final Button bt8 = (Button) findViewById(R.id.btp8);
+	        bt8.setOnClickListener(new Button.OnClickListener() {
+				@Override	
+				public void onClick(View v) {
+					String texto = bt8.getText().toString();
+					Intent i = new Intent(MainActivity.this, ChamaPlayer.class);
+					
+					i.putExtra("modalidade", texto);
+					startActivity(i);
+				} 
+
+		});	
+	        
+	        
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
